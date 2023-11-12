7773004f837da7b9e6874313edc5d2c56c9b86bc
Emilio Forrer <ebonilla@applaudostudios.com>
2023-11-11 21:50:14 -0600

    ✨ Add some more keybinds

--- init.lua
+++ init.lua
@@ -26,32 +26,50 @@ vim.api.nvim_set_keymap("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", o
 vim.api.nvim_set_keymap("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts)
 vim.api.nvim_set_keymap("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
 
-return {
-  plugins = {
-    "AstroNvim/astrocommunity",
-    -- { import = "astrocommunity.pack.bash" },
-    -- { import = "astrocommunity.pack.docker" },
-    -- { import = "astrocommunity.pack.cue" },
-    { import = "astrocommunity.pack.go" },
-    -- { import = "astrocommunity.pack.helm" },
-    -- { import = "astrocommunity.pack.html-css" },
-    -- { import = "astrocommunity.pack.json" },
-    -- { import = "astrocommunity.pack.lua" },
-    -- { import = "astrocommunity.pack.markdown" },
-    -- { import = "astrocommunity.pack.nix" },
-    { import = "astrocommunity.pack.python" },
-    -- { import = "astrocommunity.pack.ruby" },
-    { import = "astrocommunity.pack.rust" },
-    -- { import = "astrocommunity.pack.svelte" },
-    -- { import = "astrocommunity.pack.tailwindcss" },
-    -- { import = "astrocommunity.pack.terraform" },
-    -- { import = "astrocommunity.pack.toml" },
-    -- { import = "astrocommunity.pack.typescript-all-in-one" },
-    -- { import = "astrocommunity.pack.vue" },
-    -- { import = "astrocommunity.pack.yaml" },
-    { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = true },
-    { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
-    { import = "astrocommunity.colorscheme.rose-pine" },
-    { import = "astrocommunity.colorscheme.catppuccin" },
-  },
-}
+-- Some ThePrimeagen binds
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', opts)
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', opts)
+vim.api.nvim_set_keymap('n', 'Q', '<nop>', opts)
+
+
+-- ---------------------- Keybindings ------------------------------
+if vim.g.vscode then
+  -- VSCode extension
+  -- vim.api.nvim_set_keymap('n', '?', "<Cmd>call VSCodeNotify('workbench.action.findInFiles', { 'query': vim.fn.expand('<cword>')})<CR>", { noremap = true, silent = true })
+
+else
+  -- ordinary Neovim
+  
+  return {
+    plugins = {
+      "AstroNvim/astrocommunity",
+      -- { import = "astrocommunity.pack.bash" },
+      -- { import = "astrocommunity.pack.docker" },
+      -- { import = "astrocommunity.pack.cue" },
+      { import = "astrocommunity.pack.go" },
+      -- { import = "astrocommunity.pack.helm" },
+      -- { import = "astrocommunity.pack.html-css" },
+      -- { import = "astrocommunity.pack.json" },
+      -- { import = "astrocommunity.pack.lua" },
+      -- { import = "astrocommunity.pack.markdown" },
+      -- { import = "astrocommunity.pack.nix" },
+      { import = "astrocommunity.pack.python" },
+      -- { import = "astrocommunity.pack.ruby" },
+      { import = "astrocommunity.pack.rust" },
+      -- { import = "astrocommunity.pack.svelte" },
+      -- { import = "astrocommunity.pack.tailwindcss" },
+      -- { import = "astrocommunity.pack.terraform" },
+      -- { import = "astrocommunity.pack.toml" },
+      -- { import = "astrocommunity.pack.typescript-all-in-one" },
+      -- { import = "astrocommunity.pack.vue" },
+      -- { import = "astrocommunity.pack.yaml" },
+      { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = true },
+      { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
+      { import = "astrocommunity.colorscheme.rose-pine" },
+      { import = "astrocommunity.colorscheme.catppuccin" },
+    },
+  } 
+end
+
+
+
