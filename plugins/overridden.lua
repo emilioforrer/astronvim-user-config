7012a7914fa41e3f2d4fb631ef5061218e791349
Emilio Forrer <dev.emilio.forrer@gmail.com>
2024-07-12 08:36:46 -0600

    ✨ Move sidebar to the right

--- plugins/overridden.lua
+++ plugins/overridden.lua
@@ -34,5 +34,12 @@ return {
             
             return opts
         end   
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = function(_, opts)
+            opts.window.position = "right"
+            return opts
+        end   
     }
 }
