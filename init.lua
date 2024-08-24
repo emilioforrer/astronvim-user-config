8a835738546e26bc7e889c8dd1e6a81491cdb31d
Emilio Forrer <dev.emilio.forrer@gmail.com>
2024-08-23 22:50:15 -0600

    ✨ Splited shared, vscode and neovim only settings

--- init.lua
+++ init.lua
@@ -1,95 +1,17 @@
+-- ---------------------- SHARED NEOVIM SETTINGS ------------------------------
+
 -- Configuration
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
--- Set indentation per language
-
--- Function to set tab settings for Go files
-local function set_go_tabs()
-  vim.bo.tabstop = 4
-  vim.bo.shiftwidth = 4
-  vim.bo.softtabstop = 4
-  vim.bo.expandtab = false
-end
-
--- Function to set tab settings for Python files
-local function set_python_tabs()
-  vim.bo.tabstop = 4
-  vim.bo.shiftwidth = 4
-  vim.bo.softtabstop = 4
-  vim.bo.expandtab = true
-end
-
--- Create an autocommand group for Go and Python settings
-vim.api.nvim_create_augroup('ProgrammingSettings', { clear = true })
-
--- Apply the tab settings for Go files
-vim.api.nvim_create_autocmd('FileType', {
-  group = 'ProgrammingSettings',
-  pattern = 'go',
-  callback = set_go_tabs,
-})
-
--- Apply the tab settings for Python files
-vim.api.nvim_create_autocmd('FileType', {
-  group = 'ProgrammingSettings',
-  pattern = 'python',
-  callback = set_python_tabs,
-})
-
 vim.o.exrc = true
 vim.o.secure = true
 vim.opt.clipboard = "unnamedplus"
 
--- Enable list mode to show whitespace characters
-vim.opt.list = true
-
--- Define the symbols for whitespace characters
-vim.opt.listchars = {
-    space = '·',       -- Character to show for space
-    tab = '→ ',        -- Character to show for tab
-    trail = '•',       -- Character to show for trailing spaces
-    eol = '↲',         -- Character to show for end of line
-}
-
--- Optionally, you can customize the appearance of the whitespace characters using highlight groups
--- For example, to change the color of the whitespace characters:
-vim.cmd('highlight SpecialKey ctermfg=darkgray guifg=darkgray')
-vim.cmd('highlight NonText ctermfg=darkgray guifg=darkgray')
-vim.cmd('highlight EndOfBuffer ctermfg=darkgray guifg=darkgray')
-
--- Function to enable list mode
-local function enable_list_mode()
-  vim.opt.list = true
-end
-
--- Function to disable list mode
-local function disable_list_mode()
-  vim.opt.list = false
-end
-
--- Autocommands to toggle list mode based on the mode
-vim.api.nvim_create_autocmd('InsertEnter', {
-  pattern = '*',
-  callback = disable_list_mode
-})
-
-vim.api.nvim_create_autocmd('InsertLeave', {
-  pattern = '*',
-  callback = enable_list_mode
-})
-
--- Initially enable list mode when starting in normal mode
-enable_list_mode()
-
 -- ---------------------- Keybindings ------------------------------
 
-vim.api.nvim_set_keymap("n", "<C-n>", "<Plug>(multicursor-next)", {})
-vim.api.nvim_set_keymap("n", "<C-p>", "<Plug>(multicursor-prev)", {})
-vim.api.nvim_set_keymap("n", "<C-b>", "<C-V>", { noremap = true })
 
 -- Mapping for CTRL+a to select all text in normal, insert, and visual mode
 vim.api.nvim_set_keymap('n', '<C-a>', 'ggVG', { noremap = true })
@@ -128,19 +50,15 @@ vim.api.nvim_set_keymap("n", "<BS>", '"_d', { noremap = true })
 -- vim.api.nvim_set_keymap("v", "dd", '"_dd', { noremap = true })
 vim.api.nvim_set_keymap("v", "<BS>", '"_d', { noremap = true })
 
--- Copy to clipboard
-vim.api.nvim_set_keymap("n", "<leader>Y", '"+y', { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>YY", '"+yg_', { noremap = true })
-vim.api.nvim_set_keymap("v", "<leader>Y", '"+y', { noremap = true })
+-- -- Copy to clipboard
+-- vim.api.nvim_set_keymap("n", "<leader>Y", '"+y', { noremap = true })
+-- vim.api.nvim_set_keymap("n", "<leader>YY", '"+yg_', { noremap = true })
+-- vim.api.nvim_set_keymap("v", "<leader>Y", '"+y', { noremap = true })
 
--- Paste from clipboard
-vim.api.nvim_set_keymap("n", "<leader>PP", '"+P', { noremap = true })
-vim.api.nvim_set_keymap("v", "<leader>PP", '"+p', { noremap = true })
+-- -- Paste from clipboard
+-- vim.api.nvim_set_keymap("n", "<leader>PP", '"+P', { noremap = true })
+-- vim.api.nvim_set_keymap("v", "<leader>PP", '"+p', { noremap = true })
 
--- Icon picker
--- vim.api.nvim_set_keymap("n", "<C-i>", "<cmd>IconPickerNormal<cr>", opts)
-vim.api.nvim_set_keymap("n", "<C-\\><C-i>", "<cmd>IconPickerYank<cr>", opts)
-vim.api.nvim_set_keymap("i", "<C-\\><C-i>", "<cmd>IconPickerInsert<cr>", opts)
 
 -- Some ThePrimeagen binds
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', opts)
@@ -148,13 +66,100 @@ vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', opts)
 vim.api.nvim_set_keymap('n', 'Q', '<nop>', opts)
 
 
--- ---------------------- Keybindings ------------------------------
 if vim.g.vscode then
+  -- ---------------------- VSCODE NEOVIM SETTINGS------------------------------
   -- VSCode extension
   -- vim.api.nvim_set_keymap('n', '?', "<Cmd>call VSCodeNotify('workbench.action.findInFiles', { 'query': vim.fn.expand('<cword>')})<CR>", { noremap = true, silent = true })
 
 else
-  -- ordinary Neovim
+  -- ---------------------- NEOVIM ONLY SETTINGS------------------------------
+
+  -- Set indentation per language
+
+  -- Function to set tab settings for Go files
+  local function set_go_tabs()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = false
+  end
+
+  -- Function to set tab settings for Python files
+  local function set_python_tabs()
+    vim.bo.tabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.softtabstop = 4
+    vim.bo.expandtab = true
+  end
+
+  -- Create an autocommand group for Go and Python settings
+  vim.api.nvim_create_augroup('ProgrammingSettings', { clear = true })
+
+  -- Apply the tab settings for Go files
+  vim.api.nvim_create_autocmd('FileType', {
+    group = 'ProgrammingSettings',
+    pattern = 'go',
+    callback = set_go_tabs,
+  })
+
+  -- Apply the tab settings for Python files
+  vim.api.nvim_create_autocmd('FileType', {
+    group = 'ProgrammingSettings',
+    pattern = 'python',
+    callback = set_python_tabs,
+  })
+
+  -- Keybindings for multicursor
+  vim.api.nvim_set_keymap("n", "<C-n>", "<Plug>(multicursor-next)", {})
+  vim.api.nvim_set_keymap("n", "<C-p>", "<Plug>(multicursor-prev)", {})
+  vim.api.nvim_set_keymap("n", "<C-b>", "<C-V>", { noremap = true })
+
+  -- Icon picker
+  -- vim.api.nvim_set_keymap("n", "<C-i>", "<cmd>IconPickerNormal<cr>", opts)
+  vim.api.nvim_set_keymap("n", "<C-\\><C-i>", "<cmd>IconPickerYank<cr>", opts)
+  vim.api.nvim_set_keymap("i", "<C-\\><C-i>", "<cmd>IconPickerInsert<cr>", opts)
+  
+  -- Enable list mode to show whitespace characters
+  vim.opt.list = true
+
+  -- Define the symbols for whitespace characters
+  vim.opt.listchars = {
+      space = '·',       -- Character to show for space
+      tab = '→ ',        -- Character to show for tab
+      trail = '•',       -- Character to show for trailing spaces
+      eol = '↲',         -- Character to show for end of line
+  }
+
+  -- Optionally, you can customize the appearance of the whitespace characters using highlight groups
+  -- For example, to change the color of the whitespace characters:
+  vim.cmd('highlight SpecialKey ctermfg=darkgray guifg=darkgray')
+  vim.cmd('highlight NonText ctermfg=darkgray guifg=darkgray')
+  vim.cmd('highlight EndOfBuffer ctermfg=darkgray guifg=darkgray')
+
+  -- Function to enable list mode
+  local function enable_list_mode()
+    vim.opt.list = true
+  end
+
+  -- Function to disable list mode
+  local function disable_list_mode()
+    vim.opt.list = false
+  end
+
+  -- Autocommands to toggle list mode based on the mode
+  vim.api.nvim_create_autocmd('InsertEnter', {
+    pattern = '*',
+    callback = disable_list_mode
+  })
+
+  vim.api.nvim_create_autocmd('InsertLeave', {
+    pattern = '*',
+    callback = enable_list_mode
+  })
+
+  -- Initially enable list mode when starting in normal mode
+  enable_list_mode()
+
 end
 
 return {
